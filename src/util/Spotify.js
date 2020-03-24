50628544725489321c629cd38f092a19f31a0084
aron <aronphong@gmail.com>
2020-03-24 06:35:58 -0700

    Implement Spotify Search Request

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,4 @@
-const cliendId = "7886e54bd9cc4385a7b364d3fe91c805";
+const clientID = "7886e54bd9cc4385a7b364d3fe91c805";
 const redirectURI = "http://localhost:3000/";
 let token;
 const authEndpoint = 'https://accounts.spotify.com/authorize';
@@ -19,12 +19,32 @@ const Spotify = {
             window.history.pushState('Access Token', null, '/');
             return token;
         } else {
-            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
             window.location = accessUrl;
-        }
+        };
+    },
 
-    }
-
+    search(term) {
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (!jsonResponse.tracks) {
+                return [];
+            }
+            return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artist: track.artists[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }));
+        });
+    },
 }
 
 export default Spotify;
