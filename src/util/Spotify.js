e372d7dbd586be17dcef7e2069a0b3b26a6d59d5
aron <aronphong@gmail.com>
2020-03-24 20:17:24 -0700

    Refactor search to async / await

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -25,24 +25,30 @@ const Spotify = {
 
     search(term) {
         const accessToken = Spotify.getAccessToken();
-        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        }).then(response => {
-            return response.json();
-        }).then(jsonResponse => {
-            if (!jsonResponse.tracks) {
-                return [];
-            }
-            return jsonResponse.tracks.items.map(track => ({
-                id: track.id,
-                name: track.name,
-                artist: track.artists[0].name,
-                album: track.album.name,
-                uri: track.uri
-            }));
-        });
+
+        const searchRequest = async () => {
+            try {
+                const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+                    headers: { Authorization: `Bearer ${accessToken}` }
+                });
+                const jsonResponse = await response.json();
+    
+                if (!jsonResponse.tracks) {
+                    return [];
+                };
+
+                return await jsonResponse.tracks.items.map(track => ({
+                    id: track.id,
+                    name: track.name,
+                    artist: track.artists[0].name,
+                    album: track.album.name,
+                    uri: track.uri
+                }));
+            } catch (error) {
+                console.log(error);
+            };
+        };
+        return searchRequest();
     },
 
     savePlaylist(name, arrTrackURI) {
@@ -80,7 +86,6 @@ const Spotify = {
                 body: JSON.stringify({uris: arrTrackURI})
             });
         };
-
         save();
     }
 }
