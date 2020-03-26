9c77de6f1c1060c89bf40b4dbf40ad291e9e68af
aron <aronphong@gmail.com>
2020-03-25 19:19:26 -0700

    Remove auto invoked function

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -8,14 +8,6 @@ export class Track extends React.Component {
         this.removeTrack = this.removeTrack.bind(this);
     }
 
-    renderAction() {
-        if (this.props.isRemoval) {
-            return <button className="Track-action" onClick={this.removeTrack}>-</button>
-        } else {
-            return <button className="Track-action" onClick={this.addTrack}>+</button>
-        };
-    }
-
     removeTrack() {
         this.props.onRemove(this.props.track)
     }
@@ -25,13 +17,20 @@ export class Track extends React.Component {
     }
 
     render() {
+        let button;
+        if (this.props.isRemoval) {
+            button = <button className="Track-action" onClick={this.removeTrack}>-</button>
+        } else {
+            button = <button className="Track-action" onClick={this.addTrack}>+</button>
+        }
+
         return (
             <div className="Track">
                 <div className="Track-information">
                     <h3>{this.props.name}</h3>
                     <p>{this.props.artist} | {this.props.album}</p>
                 </div>
-                {this.renderAction()}
+                {button}
             </div>
         );
     }
