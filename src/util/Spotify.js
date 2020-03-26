7d80ac6b608a9ab11db5a40f8cee446d660796de
aron <aronphong@gmail.com>
2020-03-25 20:10:17 -0700

    Tern search into async function

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -6,7 +6,7 @@ const Spotify = {
     getAccessToken() {
         if (token) {
             return token;
-        }
+        };
 
         const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
         const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
@@ -23,41 +23,36 @@ const Spotify = {
         };
     },
 
-    search(term) {
+    async search(term) {
         const accessToken = Spotify.getAccessToken();
-
-        const searchRequest = async () => {
-            try {
-                const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
-                    headers: { Authorization: `Bearer ${accessToken}` }
-                });
-                const jsonResponse = await response.json();
-    
-                if (await jsonResponse.tracks) {
-                    return jsonResponse.tracks.items.map(track => ({
-                        id: track.id,
-                        name: track.name,
-                        artist: track.artists[0].name,
-                        album: track.album.name,
-                        uri: track.uri
-                    }));
-                } else {
-                    return [];
-                };
-            } catch (error) {
-                console.log(error);
-            };
+        try {
+            const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+                headers: { Authorization: `Bearer ${accessToken}` }
+            });
+            const jsonResponse = await response.json();
+            if (jsonResponse.tracks) {
+                return jsonResponse.tracks.items.map(track => ({
+                    id: track.id,
+                    name: track.name,
+                    artist: track.artists[0].name,
+                    album: track.album.name,
+                    uri: track.uri
+                }));
+            } else {
+                return [];
+            }        
+        } catch (error) {
+            console.log(error);
         };
-        return searchRequest();
     },
 
     savePlaylist(name, arrTrackURI) {
         if (!name || arrTrackURI.length === 0) {
-            return
+            return;
         }
 
         const accessToken = Spotify.getAccessToken();
-        const headers =  { Authorization: `Bearer ${accessToken}` }
+        const headers =  { Authorization: `Bearer ${accessToken}` };
         let userID = "";
 
         const save = async () => {
@@ -89,7 +84,7 @@ const Spotify = {
                 console.log(error);
             };
         };
-        save();
+        return save();
     }
 }
 
